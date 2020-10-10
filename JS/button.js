7d008a54b16fec4dc837fe25170ce2ed51571b18
lucaxgomex <lgomes15bk@gmail.com>
2020-10-09 21:31:04 -0300

    Adição de função para receber o valor de uma string e criptografá-la.

--- JS/button.js
+++ JS/button.js
@@ -1,8 +1,31 @@
-/*
-let s = document.getElementById("string");
-let n = document.getElementById("number");
-*/
-
 function showResult() {
-    window.alert("Hello World");   
+    var stringValue = getString();
+    var numberValue = getNumber();
+    var result = ceaserCipher(stringValue, numberValue);
+    document.form.display.value = result;
+}
+
+function getString() {
+    var s = document.getElementById("string").value;
+    return s;
+}
+
+function getNumber() {
+    var n = document.getElementById("number").value;
+    return parseInt(n);
+}
+
+function ceaserCipher(name, rotation) {
+    var i;
+    var newText = "";   
+    var newChar = 0;
+
+    for (i = 0; i < name.length; i++) {
+        if (name.charCodeAt(i) >= 97 && name.charCodeAt(i) <= 122
+        || name.charCodeAt(i) >= 65 && name.charCodeAt(i) <= 90) {
+            newChar = String.fromCharCode(name.charCodeAt(i) + rotation);
+            newText += newChar;
+        }
+    }
+    return newText;
 }
