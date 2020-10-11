828cf764a9952c00eed1d7822f971e94fbd32c38
lucaxgomex <lgomes15bk@gmail.com>
2020-10-11 00:54:22 -0300

    Modularização para definir quais letras serão alteradas.

--- JS/button.js
+++ JS/button.js
@@ -18,14 +18,45 @@ function getNumber() {
 function ceaserCipher(name, rotation) {
     var i;
     var newText = "";   
-    var newChar = 0;
+    var newChar;
 
     for (i = 0; i < name.length; i++) {
-        if (name.charCodeAt(i) >= 97 && name.charCodeAt(i) <= 122
-        || name.charCodeAt(i) >= 65 && name.charCodeAt(i) <= 90) {
+        if (letter) {
             newChar = String.fromCharCode(name.charCodeAt(i) + rotation);
-            newText += newChar;
+            newText = newText.concat(newChar);
+        } else {
+            if (name.charCodeAt(i) == 32) {
+                newText = newText.concat('#');
+            } else {
+                newText = newText.concat(name[i]);
+            }
         }
     }
     return newText;
+}
+
+let letter = function getLetters(name) {
+    var j;
+    for (j = 0; j < name.length; j++) {
+        if (returnUppercase(name) || returnLowercase(name)) {
+            return true; 
+            /*
+            retorne verdadeiro caso os caso
+            os caracteres da string informada sejam 
+            somente letras
+            */
+        } else {
+            return false;
+        }
+    }
+}
+
+//retorno das letras maiusculas de acordo com a tabela ASCII
+function returnUppercase(name) {
+    return name.charCodeAt(i) >= 97 && name.charCodeAt(i) <= 122;
+}
+
+//retorno das letras minusculas de acordo com a tabela ASCII
+function returnLowercase(name) {
+    return name.charCodeAt(i) >= 65 && name.charCodeAt(i) <= 90;
 }
